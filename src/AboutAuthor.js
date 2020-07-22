2282660e6c789c3f41261c59624c5a7741330055
Muxa <balashovmb@gmail.com>
2020-07-22 16:21:26 +0500

    Required task 1 done

---
+++ src/AboutAuthor.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class AboutAuthor extends React.Component {
+  render() {
+    const {
+      author: { Name, Email, Info, Avatar }
+    } = this.props
+
+    return (
+      <div style={styles.container}>
+        <div style={styles.imageBox}>
+          <img style={styles.image} src={Avatar} alt={Name}></img>
+        </div>
+        <div style={styles.textContainer}>
+          <div>{Name}</div>
+          <div>{Email}</div>
+          <div>{Info}</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const styles = {
+  container: {
+    display: 'flex'
+  },
+  imageBox: {
+    maxWidth: '100px'
+  },
+  image: {
+    width: '100%'
+  },
+  textContainer: {
+    flex: '1'
+  }
+}
+
+export default AboutAuthor;
