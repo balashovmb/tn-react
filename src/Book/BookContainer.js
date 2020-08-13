a807f8280d7deeeedee54f3f0caa6511cd942ae1
balashovmb <balashovmb@gmail.com>
2020-08-13 12:38:36 +0500

    Refactoring. BookContainer to functional component

--- src/Book/BookContainer.js
+++ src/Book/BookContainer.js
@@ -5,42 +5,41 @@ import Book from './Book';
 import withBooks from '../HOC/withBooks';
 import withLoader from '../HOC/withLoader';
 
-class BookContainer extends React.Component {
-  _mapFromAirtable(records) {
-    const record = records[0].data;
-    const authors = _.zip(
-      record.fields.Authors,
-      record.fields["Name (from Authors)"],
-      record.fields["Info (from Authors)"],
-      record.fields["AvatarUrl (from Authors)"],
-      record.fields["Email (from Authors)"]
-    ).map(record => _.zipObject(
-      ['Id', 'Name', 'Info', 'AvatarUrl', 'Email'],
-      record
-    ))
-    return ({
-      Title: record.fields.Title,
-      Annotation: record.fields.Annotation,
-      Pages: record.fields.Pages,
-      Language: record.fields.Language,
-      Progress: record.fields.Progress,
-      Cover: record.fields.Cover,
-      MinimalPrice: record.fields.MinimalPrice,
-      ExpectedPrice: record.fields.ExpectedPrice,
-      Amount: record.fields.Amount,
-      ExpectedAmount: record.fields.ExpectedAmount,
-      Subscribers: record.fields.Subscribers,
-      Authors: authors,
-      SimilarBooksIds: record.fields.SimilarBooks
-    })
-  }
+function _mapFromAirtable(records) {
+  const record = records[0].data;
+  const authors = _.zip(
+    record.fields.Authors,
+    record.fields["Name (from Authors)"],
+    record.fields["Info (from Authors)"],
+    record.fields["AvatarUrl (from Authors)"],
+    record.fields["Email (from Authors)"]
+  ).map(record => _.zipObject(
+    ['Id', 'Name', 'Info', 'AvatarUrl', 'Email'],
+    record
+  ))
+  return ({
+    Title: record.fields.Title,
+    Annotation: record.fields.Annotation,
+    Pages: record.fields.Pages,
+    Language: record.fields.Language,
+    Progress: record.fields.Progress,
+    Cover: record.fields.Cover,
+    MinimalPrice: record.fields.MinimalPrice,
+    ExpectedPrice: record.fields.ExpectedPrice,
+    Amount: record.fields.Amount,
+    ExpectedAmount: record.fields.ExpectedAmount,
+    Subscribers: record.fields.Subscribers,
+    Authors: authors,
+    SimilarBooksIds: record.fields.SimilarBooks
+  })
+}
 
-  render() {
-    const book = this._mapFromAirtable(this.props.bookRecords)
-    return (
-      <Book book={book} />
-    )
-  }
+
+const BookContainer =({bookRecords})=> {
+  const book = _mapFromAirtable(bookRecords);
+  return (
+    <Book book={book} />
+  )
 }
 
 export default withBooks(withLoader(BookContainer));
