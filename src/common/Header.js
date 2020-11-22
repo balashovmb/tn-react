c1af223c5b4f195da4c75096fbf54c822438d485
Muxa <balashovmb@gmail.com>
2020-11-22 11:54:37 +0500

    Routing tests

--- src/common/Header.js
+++ src/common/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import UserInfo from '../User/UserInfo';
 import ThemeToggle from './ThemeToggle';
+import { newBookPath, wishlistPath } from '../helpers/routes';
 
 const Header = () => (
   <header className="h-20 border bg-primary">
@@ -13,6 +14,12 @@ const Header = () => (
         <ThemeToggle />
       </div>
       <UserInfo />
+    </div>
+    <div className="float-right">
+      <Link to={wishlistPath()}>Список желаемого</Link>
+    </div>
+    <div className="float-right mr-2">
+      <Link to={newBookPath()}>Добавить книгу</Link>
     </div>
 
   </header>
